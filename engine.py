a86287ebf8e9f5fbeceda53b227beddf7d4720fd
JuliustheCreator <Jrbroomfield1@gmail.com>
2022-08-26 20:50:27 -0400

    added chess

--- engine.py
+++ engine.py
@@ -3,10 +3,10 @@ import pandas as pd
 import numpy as np
 import requests
 from chessdotcom import get_player_stats, get_player_game_archives
-import pprint
+import chess.pgn
+from io import StringIO
 
 username = input("Input Player Name: ")
-printer = pprint.PrettyPrinter()
 
 #Chooses Highest Rating Between Rapid and Blitz
 def get_player_rating(username):
@@ -21,26 +21,17 @@ def get_player_rating(username):
         return ratings[0]
     return ratings[1]
 
-def get_ECO_from_pgn(pgn, index):
-    i = 0
-    ECO = ""
-    while True:
-        i += 1
-        if pgn[index] == '"':
-            return ECO
-        ECO += pgn[index + 1]
-        
-
 ECOs = []
 #Retrives Recent Games (40%) or Most Games (80%) if Under 500 Games Were Played
 def get_player_games(username):
     games = get_player_game_archives(username).json
     game_count = len(games)
+    print(game_count)
     urls = games['archives'][-1]
     games = requests.get(urls).json()
-    for game in games['games']:
-            ECOindex = game['pgn'].index('ECO')
-            ECOs.append(get_ECO_from_pgn(game['pgn'], ECOindex))
 
+def get_ECO(game):
+    pass
 
 data = pd.read_csv("games.csv")
+get_player_games(username)
