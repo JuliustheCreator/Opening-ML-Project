66d58200fd3b27a0e65de7c8da3c6c762330a04b
Julius <jrbroomfield1@gmail.com>
2023-09-16 19:29:38 -0400

    Uploaded ECO; fully incorporated into processing

--- main/processing.py
+++ main/processing.py
@@ -1,18 +1,33 @@
-import pandas as pd
 from collections import namedtuple
+from io import StringIO
+import pandas as pd
+import chess.pgn
+
+
+ECO = pd.read_csv(
+    'main\data\ECO.csv', 
+    header = None, 
+    names = ["ECO", "Opening"]
+    ).set_index('ECO').to_dict()['Opening']
 
 Values = namedtuple('values', ['frequency', 'win_rate'])
 
 def get_opening_info(game, username):
     if 'pgn' not in game:
-        return None, None
-    pgn = game['pgn'].split('\n')
-    url = next((data for data in pgn if "ECOUrl" in data), pgn[11])
-    name = url.split('/')[-1][:-2].replace('-',' ')
+      return None, None
+    
+    pgn = game['pgn']
 
-    if game['white']['username'] == username: is_win = game['white']['result'] == 'win'
-    else: is_win = game['black']['result'] == 'win'
-    return name, is_win
+    if 'ECO' not in pgn:
+      return None, None
+
+    pgn = StringIO(pgn)
+    game = chess.pgn.read_game(pgn)
+    code = game.headers['ECO']
+
+    name = ECO.get(code, "Unknown Opening")
+
+    return name, game.headers['Result'] == "1-0"
 
 def create_matrix(games, username):
     database = {}
@@ -24,6 +39,8 @@ def create_matrix(games, username):
         if is_win: database[name]['wins'] += 1   
 
     df = pd.DataFrame(database).T
+    if 'wins' not in df.columns: df['wins'] = 0
+    if 'played' not in df.columns: df['played'] = 0
     df['win_rate'] = (df['wins'] / df['played']) * 100
     df['frequency'] = (df['played'] / len(database)) * 100
 
